66dcd7e35d54eba30d1db2334bbd462ac5173558
Will_Robin <werobinson6@gmail.com>
2021-05-22 22:29:10 +0200

    Minor modifications to fit with new function framework.

--- NorthNet/simulation/integration.py
+++ NorthNet/simulation/integration.py
@@ -1,16 +1,15 @@
 from scipy.integrate import ode
 import numpy as np
 
-def integrate_model(function, S0, k, C, time, dt):
+def integrate_model(function, S0, k, time, dt):
 
     y = ode(function).set_integrator('lsoda')
     y.set_initial_value(S0)
-    y.set_f_params(k, C)
+    y.set_f_params(k)
 
     calc = []
     time_arr = []
     while y.successful() and y.t <= time:
-
         calc.append(y.integrate(y.t+dt))
         time_arr.append(y.t+dt)
 
