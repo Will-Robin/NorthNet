e9df5222fbd19ba6cae3acf7f4c67e2534332edc
Will <werobinson6@gmail.com>
2021-05-26 20:19:09 +0200

    Incomplete

--- NorthNet/information/chemical_information/chem_info.py
+++ NorthNet/information/chemical_information/chem_info.py
@@ -64,3 +64,5 @@ reaction_colours = readTwoColInfo(script_dir/'reaction_colour_assignments.csv', 
 fr_assign = readTwoColInfo(script_dir/'fragment_assignments.csv', 0, 1)
 frag_assignments = {float(f):fr_assign[f] for f in fr_assign}
 frag_colours = readTwoColInfo(script_dir/'fragment_assignments.csv', 0, 2)
+
+# with open()
