3f6f7d483c7b3472f363d1da7cd75a8969f6004e
Will <werobinson6@gmail.com>
2021-05-26 13:24:07 +0200

    Added module and imports to work with older import framework

---
+++ NorthNet/network_ops.py
@@ -0,0 +1,9 @@
+from network_manipulations.networkx_ops.attribute_mods import *
+from network_manipulations.networkx_ops.coordinates import *
+from network_manipulations.networkx_ops.network_editing import *
+
+from network_manipulations.NorthNet_ops.attributes import *
+from network_manipulations.NorthNet_ops.mapping import *
+from network_manipulations.NorthNet_ops.old_functions import *
+from network_manipulations.NorthNet_ops.processing import *
+from network_manipulations.NorthNet_ops.editing import *
