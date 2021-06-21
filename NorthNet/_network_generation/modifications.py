c6caeb413986c2f918cb05520d86de74c35fdc84
Will <werobinson6@gmail.com>
2021-06-21 19:46:54 +0200

    moved function to different module

---
+++ NorthNet/_network_generation/modifications.py
@@ -0,0 +1,19 @@
+def add_flow_terms(network, inputs):
+    '''
+    Add flow terms into network (inputs and outputs are
+    empty species and )
+    '''
+
+    add_inputs = []
+    for i in inputs:
+        r_obj = Classes.ReactionInput("{}_#0>>{}".format(i,i))
+        add_inputs.append(r_obj)
+
+    network.add_inputs(add_inputs)
+
+    add_outputs = []
+    for c in network.NetworkCompounds:
+        r_obj = Classes.ReactionOutput("{}>>Sample".format(c))
+        add_outputs.append(r_obj)
+
+    network.add_outputs(add_outputs)
--- NorthNet/model_export/model_export.py
+++ NorthNet/model_export/model_export.py
@@ -3,26 +3,6 @@ from NorthNet import info_params
 from NorthNet import Classes
 import numpy as np
 
-def add_flow_terms(network, inputs):
-    '''
-    Add flow terms into network (inputs and outputs are
-    empty species and )
-    '''
-
-    add_inputs = []
-    for i in inputs:
-        r_obj = Classes.ReactionInput("{}_#0>>{}".format(i,i))
-        add_inputs.append(r_obj)
-
-    network.add_inputs(add_inputs)
-
-    add_outputs = []
-    for c in network.NetworkCompounds:
-        r_obj = Classes.ReactionOutput("{}>>Sample".format(c))
-        add_outputs.append(r_obj)
-
-    network.add_outputs(add_outputs)
-
 def network_indices(network):
     '''
     Parameters
