a72d708ab60ecdf9e1e2c104e6e9cd90a0e5816e
Will <werobinson6@gmail.com>
2021-06-21 20:43:24 +0200

    reaction operations transferred to Classes

--- NorthNet/reaction_ops.py
+++
@@ -1,5 +0,0 @@
-from reaction_operations.attributes import *
-from reaction_operations.comparisons import *
-from reaction_operations.editing import *
-from reaction_operations.extract_rules import *
-from reaction_operations.mapping import *
