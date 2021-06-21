6b0b2e67a2f2ca5850c48bd8cc7b579aa9a33403
Will <werobinson6@gmail.com>
2021-06-21 21:00:29 +0200

    removed remaining functions to composite plots and deleted empty file

--- NorthNet/plotting/composite_plots.py
+++ NorthNet/plotting/composite_plots.py
@@ -181,3 +181,77 @@ def network_in_expr_circle(circ_data,circ_col_map, pie_rad, network, reactotypes
     ax.set_aspect('equal')
 
     return ax
+
+def create_ellipse(mean, cov,
+                  facecolor = 'k',edgecolor = 'k', **kwargs):
+
+    from matplotlib.patches import Ellipse
+    import numpy as np
+
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+
+    ellipse = Ellipse(mean, width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, edgecolor = edgecolor, **kwargs)
+
+    return ellipse
+
+def confidence_ellipse(x, y, ax = None, weights = None, n_std=3.0,
+                        facecolor='none', edgecolor = 'k',**kwargs):
+    """
+    Adapted from matplotlib documentation.
+
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    from matplotlib.patches import Ellipse
+    import matplotlib.transforms as transforms
+    import numpy as np
+    from NorthNet.file_exports.plotting import data_plotting
+
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y, aweights = weights)
+
+    ellipse = data_plotting.create_ellipse((0, 0), cov,
+                              facecolor = facecolor,
+                              edgecolor = edgecolor,
+                              **kwargs)
+
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
