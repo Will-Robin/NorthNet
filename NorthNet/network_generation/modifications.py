f8bbf81adf1d8b3468a6c1d947a7fd2b03b753a7
Will <werobinson6@gmail.com>
2021-06-21 20:08:45 +0200

    reorganised functions agglommerated into network_generation

--- NorthNet/network_generation/modifications.py
+++ NorthNet/network_generation/modifications.py
@@ -19,3 +19,57 @@ def add_flow_terms(network, inputs):
         add_outputs.append(r_obj)
 
     network.add_outputs(add_outputs)
+
+def skip_step(network,substructure):
+
+    e_rem_list = []
+    node_rem_list = []
+    new_reaction_list = []
+
+    for r in network.NetworkCompounds:
+
+
+        if network.NetworkCompounds[r].Mol.HasSubstructMatch(substructure.Mol):
+            node_rem_list.append(r)
+            for i in network.NetworkCompounds[r].In:
+                e_rem_list.append(i)
+
+                in_rs = network.NetworkReactions[i].Reactants
+                outs = []
+
+                for z in network.NetworkReactions[i].Products:
+                    if z.SMILES != r:
+                        outs.append(z.SMILES)
+
+                for o in network.NetworkCompounds[r].Out:
+                    e_rem_list.append(o)
+
+                    out_ps = network.NetworkReactions[o].Products
+                    ins = []
+
+                    for z in network.NetworkReactions[o].Reactants:
+                        if z.SMILES != r:
+                            ins.append(z.SMILES)
+
+                    LHS = ".".join(ins+in_rs)
+                    RHS = ".".join(outs+out_ps)
+                    re_str = "{}>>{}".format(LHS,RHS)
+
+                    new_reaction_list.append( Generated_Reaction( Chem.ReactionFromSmiles(re_str), "skip_{}".format(substructure) ) )
+
+    network.add_reactions(new_reaction_list)
+
+    for s in set(e_rem_list):
+        del network.NetworkReactions[s]
+        for n in network.NetworkCompounds:
+            for x in network.NetworkCompounds[n].In:
+                if x == s:
+                    network.NetworkCompounds[n].In.remove(s)
+            for x in network.NetworkCompounds[n].In:
+                if x == s:
+                    network.NetworkCompounds[n].Out.remove(s)
+
+    for n in set(node_rem_list):
+        del network.NetworkCompounds[n]
+
+    return 0
--- NorthNet/network_generation/utilities.py
+++ NorthNet/network_generation/utilities.py
@@ -1,6 +1,6 @@
 def cleanup(reactions):
     from rdkit import Chem
-    
+
     '''Used to clear up the output of rdkit reaction function, parsing multiple
     reaction outcomes. Not perfect.'''
 
@@ -34,3 +34,48 @@ def cleanup(reactions):
         reactions_out.append(ins.strip('.'))
 
     return reactions_out
+
+def remove_network_symmetry(network):
+    """
+    Removes left handed sugars from the network based on the rule:
+    if the stereocentre furthest from the carbonyl is S, delete the species and
+    its associated reactions from the network. Furthest from the carbonyl is
+    defined for now as the last carbon in the canonicalised structure.
+    Parameters
+    ----------
+    Returns
+    -------
+    None
+    """
+    node_remove = []
+    edge_remove = []
+
+    for n in [*network.NetworkCompounds]:
+
+        ch_centres = Chem.FindMolChiralCenters(network.NetworkCompounds[n].Mol)
+
+        if len(ch_centres) > 0 and ch_centres[-1][-1] == "S":
+            node_remove.append(n)
+            #print(ch_centres)
+            for r in network.NetworkCompounds[n].In:
+                edge_remove.append(r)
+
+            for r in network.NetworkCompounds[n].Out:
+                edge_remove.append(r)
+
+    node_remove = list(set(node_remove))
+    edge_remove = list(set(edge_remove))
+
+    for r in edge_remove:
+        for re in network.NetworkReactions[r].Reactants:
+            while r in network.NetworkCompounds[re].Out:
+                network.NetworkCompounds[re].Out.remove(r)
+        for p in network.NetworkReactions[r].Products:
+            while r in network.NetworkCompounds[p].In:
+                network.NetworkCompounds[p].In.remove(r)
+
+    for r in edge_remove:
+        del network.NetworkReactions[r]
+
+    for n in node_remove:
+        del network.NetworkCompounds[n]
