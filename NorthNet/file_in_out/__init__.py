7f4182f1fa65529b419c92783430766b5fc15a09
Will <werobinson6@gmail.com>
2021-06-21 20:01:46 +0200

    changed module name

--- NorthNet/file_in_out/__init__.py
+++ NorthNet/file_in_out/__init__.py
@@ -1,15 +1,2 @@
-import numpy as np
-import os
-import networkx as nx
-import pickle
-from rdkit import Chem
-from pathlib import Path
-
-from NorthNet import Classes
-from NorthNet import info_params
-
-from NorthNet.file_loads.data_loads import *
-from NorthNet.file_loads.info_loads import *
-from NorthNet.file_loads.network_file_parsing import *
-from NorthNet.file_loads.reaction_list_parsing import *
-from NorthNet.file_loads.text_parsing import *
+from file_loads import *
+from file_exports import *
