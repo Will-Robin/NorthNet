c3e979e9d4aac2b0a7fd08e5efc51e10c55481bf
Will <werobinson6@gmail.com>
2021-06-21 20:44:01 +0200

    network operations no longer exists

--- NorthNet/network_ops.py
+++
@@ -1,9 +0,0 @@
-from network_manipulations.networkx_ops.attribute_mods import *
-from network_manipulations.networkx_ops.coordinates import *
-from network_manipulations.networkx_ops.network_editing import *
-
-from network_manipulations.NorthNet_ops.attributes import *
-from network_manipulations.NorthNet_ops.mapping import *
-from network_manipulations.NorthNet_ops.old_functions import *
-from network_manipulations.NorthNet_ops.processing import *
-from network_manipulations.NorthNet_ops.editing import *
