803980a4cd82d161c127baeea42762505aff89b9
Will <werobinson6@gmail.com>
2021-06-28 23:23:04 +0200

    refactored code

--- NorthNet/network_generation/molecule_operations/editing.py
+++ NorthNet/network_generation/molecule_operations/editing.py
@@ -8,26 +8,6 @@ def canonicalise(smiles):
     mol = Chem.MolFromSmiles(smiles)
     return Chem.MolToSmiles(mol, isomericSmiles = True)
 
-def mirror(compound):
-    '''
-    Reverses the chirality of a NorthNet Compound object.
-
-    Parameters
-    ----------
-    compound: NorthNet compound object
-        Compound to be mirrored. Modifed in place.
-    Returns
-    -------
-    None
-    '''
-
-    m1 = compound.SMILES.replace("@@","ZY")
-    m1 = m1.replace("@","@@")
-    m1 = m1.replace("ZY","@")
-
-    compound.Mol = Chem.MolFromSmiles(m1)
-    compound.SMILES = Chem.MolToSmiles(compound.Mol, isomericSmiles = True)
-
 def mirror_smiles(smiles):
     '''
     Reverses enantiomers via string replacement in SMILES
@@ -47,6 +27,24 @@ def mirror_smiles(smiles):
     m1 = m1.replace("ZY","@")
 
     return m1
+    
+def mirror(compound):
+    '''
+    Reverses the chirality of a NorthNet Compound object.
+
+    Parameters
+    ----------
+    compound: NorthNet compound object
+        Compound to be mirrored. Modifed in place.
+    Returns
+    -------
+    None
+    '''
+
+    m1 = mirror_smiles(compound.SMILES)
+
+    compound.Mol = Chem.MolFromSmiles(m1)
+    compound.SMILES = Chem.MolToSmiles(compound.Mol, isomericSmiles = True)
 
 def incorrect_chiral_H_solve(mol):
     '''
