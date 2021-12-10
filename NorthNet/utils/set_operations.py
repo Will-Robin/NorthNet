f9dea58ac3f27898cced18360dcb82d9088e0896
Will <werobinson6@gmail.com>
2021-12-10 12:10:23 +0100

    remove unused functions

--- NorthNet/utils/set_operations.py
+++
@@ -1,9 +0,0 @@
-def jaccard_similarity(list1, list2):
-    s1 = set(list1)
-    s2 = set(list2)
-    return len(s1.intersection(s2)) / len(s1.union(s2))
-
-def overlap_coefficient(list1, list2):
-    s1 = set(list1)
-    s2 = set(list2)
-    return len(s1.intersection(s2)) / min(len(list1),len(list2))
