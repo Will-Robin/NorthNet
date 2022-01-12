06beb0c58e1641a8a8523b4b10b16e5b93aa1a83
Will <werobinson6@gmail.com>
2022-01-12 21:08:13 +0100

    update imports

--- NorthNet/text_parsing/__init__.py
+++ NorthNet/text_parsing/__init__.py
@@ -1,3 +1,4 @@
-from NorthNet.text_parsing.compound_list_parsing import *
-from NorthNet.text_parsing.network_file_parsing import *
-from NorthNet.text_parsing.reaction_list_parsing import *
+from .compound_list_parsing import *
+from .network_file_parsing import *
+from .reaction_list_parsing import *
+from .conversions import *
