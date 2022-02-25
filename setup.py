f839f7178de4bd4786087fe8e4673a2cd283e060
W_E_Robinson <wills_email>
2022-02-25 09:19:16 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     version="0.2",
     author="William E. Robinson",
     packages = ["NorthNet"],
+    install_requires=[
+        "networkx >= 2.6.3",
+        "numpy >= 1.21.5",
+        "rdkit >= 2021.09.4",
+        "scipy >= 1.7.3"
+    ],
 )
