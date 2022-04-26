c5ca8d036e56bad1a34f0bb0488b852e30dac34f
Will <werobinson6@gmail.com>
2022-04-26 22:09:45 +0200

    create reaction operations module

---
+++ NorthNet/reaction_operations/editing.py
@@ -0,0 +1,27 @@
+from NorthNet.text_parsing import conversions as conv
+from NorthNet import molecule_operations as mol_ops
+
+def canonicalise_reaction_smiles(reaction_smiles):
+    """
+    Canonicalise the consituent SMILES of a reaction SMILES string.
+
+    Parameters
+    ----------
+    reaction_smiles: str
+
+    Returns
+    -------
+    canonicalised_rxn_smiles: str
+    """
+
+    reactants, products = conv.reaction_smiles_split(reaction_smiles)
+
+    canonical_reacts = [mol_ops.canonicalise(r) for r in reactants]
+    canonical_prods = [mol_ops.canonicalise(p) for p in products]
+
+    lhs = ".".join(canonical_reacts)
+    rhs = ".".join(canonical_prods)
+
+    canonicalised_rxn_smiles = f"{lhs}>>{rhs}"
+
+    return canonicalised_rxn_smiles
