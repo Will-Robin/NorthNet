b98cc53f63e9fcf44a07f312604459f4ebdddb96
Will <werobinson6@gmail.com>
2022-04-28 22:34:32 +0200

    bump up minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, version
 
 setup(
     name="NorthNet",
-    version="0.2",
+    version="0.2.1",
     author="William E. Robinson",
     packages = ["NorthNet"],
     install_requires=[
