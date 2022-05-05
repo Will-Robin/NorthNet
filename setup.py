71b882595da16e84280d42b0c3aa285ec8157ccb
Will <werobinson6@gmail.com>
2022-05-05 15:56:50 +0200

    add setup.py with no dependencies for pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, version
+
+setup(
+    name="NorthNet",
+    version="0.2.1",
+    author="William E. Robinson",
+    packages = ["NorthNet"],
+    install_requires=[
+    ],
+)
