ae35edaa9670b6000ed1431bf84c5932ca69c8dc
Will <werobinson6@gmail.com>
2022-05-22 15:51:56 +0200

    add experimental data to the model

--- scripts/example_tellurium_model.py
+++ scripts/example_tellurium_model.py
@@ -1,8 +1,10 @@
 from NorthNet.Classes import Network
 from NorthNet.Classes import ModelWriter
+from NorthNet.Classes import DataReport
 from NorthNet.text_parsing import load_network_from_reaction_list
 
 reaction_file = "exampleReactionList.txt"
+data_file = "exampleData.csv"
 
 with open(reaction_file, "r") as file:
     text = file.read()
@@ -10,8 +12,9 @@ with open(reaction_file, "r") as file:
 reaction_list = [x for x in text.split("\n") if x != ""]
 
 network = load_network_from_reaction_list(reaction_list)
+experiment = DataReport(file=data_file)
 
-model = ModelWriter(network=network)
+model = ModelWriter(network=network, experiment=experiment)
 
 tellurium_model = model.to_tellurium_model()
 
