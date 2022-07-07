e7d58af875405a345a0824c61e6525618ea3d152
Will <werobinson6@gmail.com>
2022-07-07 15:57:19 +0200

    update docs

--- NorthNet/__init__.py
+++ NorthNet/__init__.py
@@ -1,9 +1,4 @@
 """
-This documentation is a work in progress, as is NorthNet in general.
-
-Significant rearrangements may be made to make the repository easier to
-navigate.
-
 This documentation was generated from the NorthNet source files using
 [pdoc](https://pdoc.dev).
 """
